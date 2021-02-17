1a4ab6700ce1b2926e640709411124620a51eda3
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2021-02-17 04:03:21 +0100

    add forgotten realms in translation.js

--- src/translation.js
+++ src/translation.js
@@ -3,7 +3,13 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import XHR from "i18next-xhr-backend";
 
 import translationEn from "./locales/en/translation.json";
+import translationDe from "./locales/de/translation.json";
+import translationTr from "./locales/tr/translation.json";
 import translationEs from "./locales/es/translation.json";
+import translationFr from "./locales/fr/translation.json";
+import translationIt from "./locales/it/translation.json";
+import translationJp from "./locales/jp/translation.json";
+
 
 i18n
   .use(XHR)
@@ -22,8 +28,23 @@ i18n
       en: {
         translations: translationEn,
       },
+      de: {
+        translations: translationDe,
+      },
+      tr: {
+        translations: translationTr,
+      },
+      it: {
+        translations: translationIt,
+      },
       es: {
         translations: translationEs,
+      },
+      fr: {
+        translations: translationFr,
+      },
+      jp: {
+        translations: translationJp,
       },
     },
     ns: ["translations"],
